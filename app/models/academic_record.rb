5159fb27a539d79c86a9569bdc6996f6112b05c9
Rob Kaufman <rob@notch8.com>
2015-12-07 11:58:08 -0800

    dont require a removed field

--- app/models/academic_record.rb
+++ app/models/academic_record.rb
@@ -1,14 +1,14 @@
 class AcademicRecord < ActiveRecord::Base
   belongs_to  :user
   has_attached_file         :transcript
-  
+
 #  validates_attachment_presence :transcript
 #  validates_attachment_size :transcript, :less_than => 10.megabytes
-  validates_presence_of     :college, :college_start, :college_end, :college_level, :major, :gpa, :gpa_range, :funding
+  validates_presence_of     :college, :college_start, :college_end, :college_level, :major, :gpa, :gpa_range
   before_post_process :randomize_file_name
 
   private
-  
+
   def randomize_file_name
     unless transcript_file_name == nil
       extension = File.extname(transcript_file_name).downcase
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # create admins
-u = User.new(:firstname => 'SURF REU', :lastname => 'Admin', :login => 'jgrevich@ucsd.edu', :email => 'jgrevich@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
+u = User.new(:firstname => 'SURF REU', :lastname => 'Admin', :login => 'rob@notch8.com', :email => 'rob@notch8.com', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
 
