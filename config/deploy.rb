d985022fc1295285a67b0464cc55175368e2aff9
Rob Kaufman <rob@notch8.com>
2016-01-07 14:46:29 -0800

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ require "rvm/capistrano"
 set :application, "efore" #matches names used in smf_template.erb
 set :repository,  "https://iem.ucsd.edu/svn/nsfreu/branches/efore"
 #set :domain, "192.168.10.103"
-set :domain, "indra.ucsd.edu"
+set :domain, "indra"
 #set :deploy_to, "/var/www/#{application}" # I like this location
 set :deploy_to, "/var/www/#{application}" # I like this location
 set :user, "ubuntu"
@@ -26,13 +26,13 @@ set :thin_port, 4040
 set :thin_cmd,  "bundle exec thin"
 
 namespace :deploy do
-  
+
   desc "chown & chmod to www-data"
   task :chown do
     sudo "chown -R #{user}:www-data #{deploy_to}"
     sudo "chmod -R 770 #{deploy_to}"
   end
-  
+
   task :start, :roles => :app, :except => { :no_release => true } do
     run "cd #{current_path} && #{thin_cmd} start -e #{rails_env} -p #{thin_port} -d"
   end
