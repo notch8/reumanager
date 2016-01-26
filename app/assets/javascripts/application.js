302aa848cd4be0291c82c021b23382913f63bf74
jeffolen4 <jmo@olen-inc.com>
2016-01-26 07:59:19 -0800

    removed modernizr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,18 +31,18 @@
 
 $(document).ready(function() {
 
-if(!Modernizr.input.placeholder){
-$("input").each(
-function(){
-if($(this).val()=="" && $(this).attr("placeholder")!=""){
-$(this).val($(this).attr("placeholder"));
-$(this).focus(function(){
-if($(this).val()==$(this).attr("placeholder")) $(this).val("");
-});
-$(this).blur(function(){
-if($(this).val()=="") $(this).val($(this).attr("placeholder"));
-});
-}
-});
-}
+// if(!Modernizr.input.placeholder){
+// $("input").each(
+// function(){
+// if($(this).val()=="" && $(this).attr("placeholder")!=""){
+// $(this).val($(this).attr("placeholder"));
+// $(this).focus(function(){
+// if($(this).val()==$(this).attr("placeholder")) $(this).val("");
+// });
+// $(this).blur(function(){
+// if($(this).val()=="") $(this).val($(this).attr("placeholder"));
+// });
+// }
+// });
+// }
 });
