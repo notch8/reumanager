2d2bfb29e663fc75e02c7d47855e3928cb57e9cc
jeffolen4 <jmo@olen-inc.com>
2016-01-26 16:39:48 -0800

    where we're goingwe don't need maps

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,50 +30,18 @@ role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
-namespace :rake do
-  desc "load settings from yml files"
-  task :load_settings do
-    on roles(:app) do
-      within "#{current_path}" do
-        with rails_env: :production do
-          execute :rake, "settings:load"
-        end
-      end
-    end
-  end
-end
 
+before 'deploy:assets:precompile', 'deploy:symlink_db'
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "cd #{release_path} && touch tmp/restart.txt"
   end
 
-  desc "chown & chmod to www-data"
-  task :chown do
-    sudo "chown -R jjg:www-data #{deploy_to}"
-    sudo "chmod -R 775 #{deploy_to}"
-  end
-
-  desc "Add config dir to shared folder"
-  task :add_shared_config do
-    run "mkdir #{deploy_to}/shared/config"
-  end
-
-  desc "Symlink configs"
-  task :symlink_configs, :roles => :app do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "rm #{release_path}/config/database.yml"
     run "ln -nfs #{deploy_to}/shared/config/*.yml #{release_path}/config/"
   end
-end
 
-namespace :ckeditor do
-  desc "Symlink the Rack::Cache files"
-  task :symlink, :roles => [:app] do
-    run "mkdir -p #{shared_path}/system/ckeditor_assets && ln -nfs #{shared_path}/system/ckeditor_assets #{release_path}/system/ckeditor_assets"
-  end
 end
-# after 'deploy:update_code', 'ckeditor:symlink'
-
-before 'deploy:assets:precompile', 'deploy:symlink_configs'
-after 'deploy:setup', 'deploy:add_shared_config'
