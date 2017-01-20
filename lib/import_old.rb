d18e88d7e74c14f25f70b82b25633c1f224f78c7
David Donohue <donohue76@hotmail.com>
2017-01-19 17:15:00 -0800

    mapping mstp_surf to mstd_surf_new for importing data and configured gitignore to ignore .DS_Store

--- lib/import_old.rb
+++ lib/import_old.rb
@@ -7,6 +7,7 @@ user = Old::User.all.detect do |user| user.academic_record.present? end
 #    next unless user.academic_record.present?
 
     applicant = Applicant.new
+    old_extra = user.extra
     applicant.first_name = user.firstname
     applicant.last_name = user.lastname
     applicant.phone = user.phone
@@ -16,12 +17,38 @@ user = Old::User.all.detect do |user| user.academic_record.present? end
     applicant.gender = user.gender
     applicant.ethnicity = user.ethnicity
     applicant.race = user.race
-    applicant.academic_level = user.
+    applicant.statement = old_extra.personal_statement
+    applicant.underrepresented_eligibility = old_extra.extra_research_interests
+    applicant.past_experience = old_extra.career
+
+
+
+
     # map the rest of the user fieds
 
     academic_record = applicant.records.build
     old_academic_record = user.academic_record
     academic_record.university = old_academic_record.college
+
+    applicant.lab_skills = old_extra.lab_skills
+    applicant.cpu_skills = old_extra.comp_skills
+    applicant.academic_level = old_academic_record.college_level
+    applicant.statement = old_extra.personal_statement
+    academic_record.start = old_academic_record.college_start
+    academic_record.finish = old_academic_record.college_end
+    academic_record.gpa = old_academic_record.gpa
+    academic_record.gpa_range = old_academic_record.gpa_range
+    academic_record.gpa_comment = old_extra.gpa_comments
+    academic_record.applicant_id = old_academic_record.user_id
+    academic_record.created_at = old_academic_record.created_at
+    academic_record.updated_at = old_academic_record.updated_at
+    academic_record.transcript_file_name = old_academic_record.transcript_file_name
+    academic_record.transcript_content_type = old_academic_record.transcript_content_type
+    academic_record.transcript_file_size = old_academic_record.transcript_file_size
+    academic_record.transcript_updated_at = old_academic_record.transcript_updated_at
+
+
+
     # map the rest of the academic record fields
 
     complete = applicant.validates_application_completeness
