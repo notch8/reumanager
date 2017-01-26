00a35f078ed754cd37cd2c71273835886f96fc6a
Rob Kaufman <rob@notch8.com>
2017-01-25 16:22:05 -0800

    fix asset path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,5 @@ Reuman::Application.configure do
   config.action_mailer.smtp_settings = { :enable_starttls_auto => false, :host => 'localhost' }
   Rails.application.routes.default_url_options = { :protocol => 'https',  :host => 'reumanager.com/surf' }
 
-  config.action_controller.relative_url_root = '/SURF'
+  config.action_controller.relative_url_root = '/surf'
 end
