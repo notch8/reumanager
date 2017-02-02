6bae0acb318093582db9b2ab1d0f1e8163c3b219
Lea Ann Bradford <leaann@notch8.com>
2017-02-01 16:57:23 -0800

    made changes as requested per customer request to admin panel, form changes, fixed submit but on academic record

--- app/models/academic_record.rb
+++ app/models/academic_record.rb
@@ -1,5 +1,5 @@
 class AcademicRecord < ActiveRecord::Base
-  attr_accessible :academic_level, :degree, :finish, :gpa, :gpa_comment, :gpa_range, :start, :university, :major, :minor
+  attr_accessible :academic_level, :degree, :finish, :finish_month, :finish_year, :gpa, :gpa_comment, :gpa_range, :start, :university, :major, :minor
   belongs_to :applicant, :class_name => "Applicant", :foreign_key => "applicant_id"
   attr_accessible :transcript
   has_attached_file :transcript, :url => ":rails_relative_url_root/system/:class/:attachment/:id_partition/:style/:filename"
@@ -8,15 +8,41 @@ class AcademicRecord < ActiveRecord::Base
     :size => { :in => 0..10.megabytes }
 
   validates :university, :presence => true
-  validates :start, :presence => true
+  # validates :start, :presence => true
   validates :finish, :presence => true
   validates :gpa, :presence => true
   validates :gpa_range, :presence => true
 
   def to_s
-    record = "#{self.start.strftime("%Y.%m")} - #{self.finish.strftime("%Y.%m")} studying #{self.degree} at #{self.university}"
+    record = "studying #{self.degree} at #{self.university} until #{self.finish.strftime("%B %Y")} "
     record << "\n#{Markdown.render "GPA Comment: " + self.gpa_comment}" if self.gpa_comment
     record
+  end
+
+  def finish_year
+    @finish_year ||= finish.try(:year)
+  end
+
+  def finish_year=(value)
+    @finish_year = value
+    self.finish = if self.finish.blank?
+      Time.parse("#{value}-01-01 00:00:00")
+    else
+      Time.parse("#{value}-#{self.finish.month}-#{self.finish.day} 00:00:00")
+    end
+  end
+
+  def finish_month
+    @finish_month ||= finish.try(:month)
+  end
+
+  def finish_month=(value)
+    @finish_month = value
+    self.finish = if finish.blank?
+      Time.parse("2020-#{value}-01 00:00:00")
+    else
+      Time.parse("#{self.finish.year}-#{value}-#{self.finish.day} 00:00:00")
+    end
   end
 
 end
