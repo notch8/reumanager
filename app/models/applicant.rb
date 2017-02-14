e926ce1f1839833a4cadf2097adab7c3945f44ba
Lea Ann Bradford <leaann@notch8.com>
2017-02-14 09:29:39 -0800

    added fields to admin panel, fixed spelling in error message

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -293,7 +293,7 @@ class Applicant < ActiveRecord::Base
   def validates_personal_info
     validates_presence_of :addresses, :message => "can't be blank.  Please add at least one address to your profile."
     validates_presence_of :phone, :message => "can't be blank. Please add at least one phone number to your profile."
-    validates_presence_of :statement, :message => "can't be blank. Your personal statement needs to be at least one sentance long."
+    validates_presence_of :statement, :message => "can't be blank. Your personal statement needs to be at least one sentence long."
 
     return true if self.errors.empty?
   end
