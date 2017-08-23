e3e9db90efe5c9a674b4e7cc78728595396388c5
Amy Dyson <amy.dyson@mac.com>
2017-08-23 10:57:20 -0700

    Fixes database problems

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,8 +2,17 @@ class University < ActiveRecord::Base
   validates :subdomain, exclusion: { in: %w(www admin),
   message: "%{value} is reserved"}
   after_create :create_tenant
+  after_destroy :destroy_tenant
 
   def create_tenant
     Apartment::Tenant.create(subdomain)
+    Apartment::Tenant.switch!(subdomain)
+    Setting.load_from_yaml
+    Apartment::Tenant.switch!
   end
+
+  def destroy_tenant
+  	Apartment::Tenant.drop(subdomain)
+  end
+
 end
